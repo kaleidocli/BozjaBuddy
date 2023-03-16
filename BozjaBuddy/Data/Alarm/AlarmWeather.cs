3b2211326d975404ecbff5abd42b7ce4398edacf
kaleidocli <42382100+kaleidocli@users.noreply.github.com>
2023-03-16 12:15:49 -0700

    Added alarm stuff

---
+++ BozjaBuddy/Data/Alarm/AlarmWeather.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BozjaBuddy.Data.Alarm
+{
+    internal class AlarmWeather : Alarm
+    {
+        protected new int mTrigger { get; set; }
+
+        public AlarmWeather(int pTriggerWeatherId)
+        {
+            this.mId = _ID_COUNTER; Alarm._ID_COUNTER += 1;
+            this.mName = $"Alarm #{this.mId}";
+            this.mTrigger = pTriggerWeatherId;
+        }
+        public AlarmWeather(int pTriggerWeatherId, string pName)
+        {
+            this.mId = _ID_COUNTER; Alarm._ID_COUNTER += 1;
+            this.mName = pName;
+            this.mTrigger = pTriggerWeatherId;
+        }
+
+        public override bool CheckAlarm(Plugin pPlugin)
+        {
+            return true;
+        }
+
+            
+    }
+}
